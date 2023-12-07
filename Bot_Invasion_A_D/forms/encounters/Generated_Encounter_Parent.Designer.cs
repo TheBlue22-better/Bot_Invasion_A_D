239f57e1cb4145e40a90e4064de36de0e52c614a
Daniel Blue <XSUSD004@studenti.czu.cz>
2023-12-07 09:30:57 +0100

    bugfix

--- Bot_Invasion_A_D/forms/encounters/Generated_Encounter_Parent.Designer.cs
+++ Bot_Invasion_A_D/forms/encounters/Generated_Encounter_Parent.Designer.cs
@@ -28,31 +28,18 @@
         /// </summary>
         private void InitializeComponent()
         {
-            buttonGeneral = new Button();
             SuspendLayout();
-            // 
-            // buttonGeneral
-            // 
-            buttonGeneral.Location = new Point(402, 127);
-            buttonGeneral.Name = "buttonGeneral";
-            buttonGeneral.Size = new Size(152, 78);
-            buttonGeneral.TabIndex = 0;
-            buttonGeneral.Text = "xd general button";
-            buttonGeneral.UseVisualStyleBackColor = true;
             // 
             // Generated_Encounter_Parent
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
-            Controls.Add(buttonGeneral);
             Name = "Generated_Encounter_Parent";
             Text = "Form1";
             ResumeLayout(false);
         }
 
         #endregion
-
-        private Button buttonGeneral;
     }
 }
